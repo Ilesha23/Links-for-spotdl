a4b69ab3b30870a0ffb374959be591e344c47027
Ilya Yakovlev <Illia.Yakovliev@cs.khpi.edu.ua>
2022-04-25 19:10:28 +0300

    Username, linkLength, spotdl.exe added

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace lfs
@@ -18,18 +13,19 @@ namespace lfs
             input.Read(bytes, 0, 10000);
             string s = input.ToString();*/ // one way is to increase buffer because ReadLine is too small
 
+            int linkLength = 79;
             string s = Clipboard.GetText().ToString(); // another way is to get text right from the clipboard
             Console.WriteLine(s);
             s = s.Replace("\n", "");
 
-            int n = s.Length / 79; // 79 - legnth of the spotify link
+            int n = s.Length / linkLength; // 79 - legnth of the spotify link
             string[] temp = new string[n];
             int a = 0;
             for (int i = 0; i < n; i++)
             {
-                temp[i] = s.Substring(a, 79);
-                a += 79;
-                System.Diagnostics.Process.Start("spotdl.exe ", "-o C:\\Users\\Win10\\Desktop\\Music " + temp[i]);
+                temp[i] = s.Substring(a, linkLength);
+                a += linkLength;
+                System.Diagnostics.Process.Start(@"external\spotdl.exe", $" -o C:\\Users\\{Environment.UserName}\\Desktop\\Music {temp[i]}");
                 System.Threading.Thread.Sleep(50);
                 if ((i + 1) % 5 == 0)
                 {
